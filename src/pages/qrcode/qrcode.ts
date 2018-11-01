7420485099c3679558c0e339b0d880225a67ee50
ivshyam98 <ivshyam1998@gmail.com>
2018-11-01 22:18:40 +0530

    allow doctor write post req done

--- src/pages/qrcode/qrcode.ts
+++ src/pages/qrcode/qrcode.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { BarcodeScanner,BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
+import { HttpClient,HttpHeaders} from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+import { Observable } from 'rxjs/Observable';
+import { Headers } from '@angular/http';
 /**
  * Generated class for the QrcodePage page.
  *
@@ -16,8 +22,14 @@ import { BarcodeScanner,BarcodeScannerOptions } from '@ionic-native/barcode-scan
 export class QrcodePage {
   options:BarcodeScannerOptions;
   docId:any;
-  constructor(private barcodeScanner: BarcodeScanner,public navCtrl: NavController, public navParams: NavParams) {
-    
+  body:any = {
+    "$class": "org.med.chain.AllowDoctorWrite",
+    "patient": "pat1",
+    "doctorId": "doc1",
+    "timestamp": "2018-11-01T05:11:23.883Z"
+  }
+  constructor(public http: HttpClient,private barcodeScanner: BarcodeScanner,public navCtrl: NavController, public navParams: NavParams) {
+  
   }
 
   ionViewDidLoad() {
@@ -30,7 +42,20 @@ export class QrcodePage {
       const results = await this.barcodeScanner.scan()
       console.log(results)
       alert(results.format)
-      this.docId=results.text
+      this.docId=results.text;
+      this.body.doctorId=this.docId;
+      this.postAllowDoctorWrite()
       
+  }
+
+  postAllowDoctorWrite(){
+    let headers = new HttpHeaders();
+    headers.append('Content-Type','application/json');
+    this.http.post('http://192.168.0.9:3000/api/AllowDoctorWrite',this.body,{headers:headers}).map(res=>
+     res
+    ).subscribe(res=>{
+      console.log(res);
+    })
+    alert("hurray!")
   }
 }
